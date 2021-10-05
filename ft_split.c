362cd80954b206696b9b33d42db9f679b710dcc8
tpopescu <75141724+tpopescu4@users.noreply.github.com>
2021-10-05 08:52:40 +0200

    Split pulido

--- ft_split.c
+++ ft_split.c
@@ -14,40 +14,40 @@
 
 size_t	ft_strlen2(const char *str, char c, size_t i)
 {
-	int	init;
+	int	start;
 
-	init = i;
-	while (str[i] != '\0' && str[i] != c)
+	start = i;
+	while (str[i] && str[i] != c)
 		i++;
-	return (i - init);
+	return (i - start);
 }
 
-static	int	ft_count_w(const char *s, char c)
+int	ft_cont_p(const char *s, char c)
 {
-	int	word;
+	int	p;
 	int	i;
 
 	i = 0;
-	word = 0;
+	p = 0;
 	if (!s)
 		return (0);
 	if (s[0] != c)
-		word++;
+		p++;
 	while (s[i])
 	{
 		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-			word++;
+			p++;
 		i++;
 	}
-	if (word == 0)
+	if (p == 0)
 		return (1);
-	return (word);
+	return (p);
 }
 
-static	char	*ft_put_w(const char *s, char c, int *i)
+char	*ft_put_p(const char *s, char c, int *i)
 {
 	char	*str;
-	int		k;
+	int		j;
 
 	str = (char *)malloc(sizeof(char) * (ft_strlen2(s, c, *i) + 1));
 	if (!str)
@@ -55,11 +55,11 @@ static	char	*ft_put_w(const char *s, char c, int *i)
 	k = 0;
 	while (s[*i] != c && s[*i])
 	{
-		str[k] = s[*i];
-		k++;
+		str[j] = s[*i];
+		j++;
 		*i += 1;
 	}
-	str[k] = '\0';
+	str[j] = '\0';
 	while (s[*i] == c && s[*i])
 		*i += 1;
 	return (str);
@@ -69,24 +69,24 @@ char	**ft_split(const char *s, char c)
 {
 	int		i;
 	int		j;
-	int		word;
+	int		p;
 	char	**str;
 
 	i = 0;
 	j = 0;
-	word = ft_count_w(s, c);
+	p = ft_cont_p(s, c);
 	if (!s)
 		return (NULL);
-	str = (char **)malloc(sizeof(char *) * (ft_count_w(s, c) + 1));
+	str = (char **)malloc(sizeof(char *) * (ft_cont_p(s, c) + 1));
 	if (!str)
 		return (NULL);
 	while (s[i] == c && s[i])
 		i++;
-	while (j < word && s[i])
+	while (j < p && s[i])
 	{
-		str[j] = ft_put_w(s, c, &i);
+		str[j] = ft_put_p(s, c, &i);
 		j++;
 	}
-	str[j] = NULL;
+	str[j] = '\0';
 	return (str);
 }
