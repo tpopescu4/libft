7ed72ebf4d4195a150ef730c57a51b65e6a248b0
tpopescu <75141724+tpopescu4@users.noreply.github.com>
2021-10-05 09:01:14 +0200

    strlcpy pulido

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -20,19 +20,15 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	if (!src || !dest)
 		return (0);
 	if (size == 0)
-	{
-		while (src[i])
-			i++;
-		return (i);
-	}
-	while (i < size - 1 && src[i] != '\0')
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	if (i < size)
 		dest[i] = '\0';
-	while (src[i] != '\0')
+	while (src[i])
 		i++;
 	return (i);
 }
